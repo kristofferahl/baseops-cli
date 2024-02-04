be0e6b3752d9339e11f337207531a15e4ea4ea94
Kristoffer Ahl <mail@77dynamite.com>
2024-02-04 11:57:32 +0100

    fix: disabling tty and interactive when CI=true

--- rootfs/etc/baseops-cli/templates/commands/shell.sh
+++ rootfs/etc/baseops-cli/templates/commands/shell.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 shell() {
+  if [[ -z "${TERM}" || "${TERM}" == 'dumb' ]]; then
+    echo "WARN: The shell command requires a terminal to be present!"
+    exit 1
+  fi
   SHLVL=0 # Reset the shell level
   /etc/baseops/scripts/boot
 }
