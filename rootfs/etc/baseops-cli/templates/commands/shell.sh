86f84813fe9542d00bbd234e74c95f3a9aaa98a2
Kristoffer Ahl <mail@77dynamite.com>
2024-02-04 11:01:31 +0100

    feat(init): collecting additional cli meta
    Enabled modifying description, banner and docker image during init

--- rootfs/etc/baseops-cli/templates/commands/shell.sh
+++ rootfs/etc/baseops-cli/templates/commands/shell.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export BANNER='{{ .Env.CLI_NAME }}'
-
 shell() {
   SHLVL=0 # Reset the shell level
   /etc/baseops/scripts/boot
--- rootfs/etc/baseops-cli/templates/scripts/init.sh
+++ rootfs/etc/baseops-cli/templates/scripts/init.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+banner() {
+  export BANNER='{{ .Env.CLI_BANNER }}'
+  echo
+  echo "Welcome to ${BANNER:?}!"
+}
+
 init() {
   # Put your initialization code here
-  echo
-  echo "Initializing {{ .Env.CLI_NAME }}..."
+  banner "$@"
 }
 
 init "$@"
