c4c33f0c653c21ba38683a237b297198b7fe2853
Kristoffer Ahl <mail@77dynamite.com>
2024-02-16 16:36:46 +0100

    feat(init): displaying the version in logo header

--- rootfs/etc/baseops-cli/templates/scripts/init.sh
+++ rootfs/etc/baseops-cli/templates/scripts/init.sh
@@ -4,7 +4,7 @@ logo() {
   export BANNER='{{ .Env.CLI_BANNER }}'
   [[ "${NO_LOGO:-false}" == 'true' || "${QUIET:-false}" == 'true' ]] && return
   echo
-  gum style --foreground 10 "# I am ${BANNER:?}!"
+  gum style --foreground 10 "# I am ${BANNER:?} {{ .Env.CLI_VERSION_VAR | strings.ToUpper }}!"
   gum style --foreground 10 "# Based on baseops-cli ${BASEOPS_CLI_VERSION:?}"
 }
 
