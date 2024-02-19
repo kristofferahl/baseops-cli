6346cd1411831422e0baaa914e4c17db251dc220
Kristoffer Ahl <mail@77dynamite.com>
2024-02-19 09:44:26 +0100

    refactor(init.sh): renamed logo to init_logo and included alpine version in output

--- rootfs/etc/baseops-cli/templates/scripts/init.sh
+++ rootfs/etc/baseops-cli/templates/scripts/init.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-logo() {
+init_logo() {
   export BANNER='{{ .Env.CLI_BANNER }}'
-  [[ "${NO_LOGO:-false}" == 'true' || "${QUIET:-false}" == 'true' ]] && return
+  [[ "${NO_LOGO:-false}" == 'true' || "${QUIET:-false}" == 'true' ]] && return 0
   echo
   gum style --foreground 10 "# I am ${BANNER:?} {{ .Env.CLI_VERSION_VAR | strings.ToUpper }}!"
-  gum style --foreground 10 "# Based on baseops-cli ${BASEOPS_CLI_VERSION:?}"
+  gum style --foreground 10 "# Based on baseops-cli ${BASEOPS_CLI_VERSION:?}, running on Alpine ${BASEOPS_ALPINE_VERSION:?}"
 }
 
 init() {
   # Put your initialization code here
-  logo "$@"
+  init_logo "$@"
 }
 
 init "$@"
